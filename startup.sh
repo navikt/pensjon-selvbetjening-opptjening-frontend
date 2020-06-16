2370c5cf3a112ab92d0b169c8278a76644dc3980
Rittik Dey <rittikdey@Rittiks-MacBook-Pro.local>
2020-06-16 13:10:54 +0200

    PL-1441: Removed slassh in the beginnging of kvPath. Removed exporting *.env from vault

--- startup.sh
+++ startup.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# fetching env from vault file.
-if test -d /var/run/secrets/nais.io/vault;
-then
-    for FILE in /var/run/secrets/nais.io/vault/*.env
-    do
-        for line in $(cat ${FILE}); do
-            echo "Startup: exporting `echo ${line} | cut -d '=' -f 1`"
-            export ${line}
-        done
-    done
-fi
-
 if test -d /secret/apigw;
 then
   export API_GW_API_KEY=$(cat /secret/apigw/x-nav-apiKey)
