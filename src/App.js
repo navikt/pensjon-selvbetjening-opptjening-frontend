310fa8a111c0f6e2a35b20c62144ccaf25a96d53
Rittik Dey <rittikdey@Rittiks-MacBook-Pro.local>
2020-06-17 14:17:26 +0200

    PL-1441: Fetching data from new Pensjonspoeng endpoint

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
         )
   }, []);
 
-    if (isLoaded && opptjening.pensjonspoeng[0]) {
+    if (isLoaded && opptjening && opptjening.pensjonspoeng && opptjening.pensjonspoeng[0]) {
         return (
             <div className="App">
                 <header className="App-header">
