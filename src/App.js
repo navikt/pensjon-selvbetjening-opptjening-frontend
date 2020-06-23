57a26deeaf33bbe77df2adffbd83d8daa79bf6cc
Rittik Dey <dey.rittik@nav.no>
2020-06-23 16:55:12 +0200

    PL-1441: Minor changes to render

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
-    const [opptjening, setOpptjening] = useState({});
+    const [opptjening, setOpptjening] = useState([]);
 
     // Note: the empty deps array [] means
     // this useEffect will run once
@@ -26,7 +26,7 @@ function App() {
                         window.location.href = "https://loginservice-q.nav.no/login?redirect=https://www-q0.nav.no/pensjon/opptjening/"
                     } else if (response.status === 200) {
                         setIsLoaded(true);
-                        setOpptjening(response);
+                        setOpptjening(response.pensjonspoeng);
                     }
                 },
                 // Note: it's important to handle errors here
@@ -39,7 +39,11 @@ function App() {
             )
     }, []);
 
-    if (isLoaded && opptjening && opptjening.pensjonspoeng && opptjening.pensjonspoeng[0]) {
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
         return (
             <div className="App">
                 <header className="App-header">
@@ -55,15 +59,43 @@ function App() {
                     >
                         Learn React
                     </a>
-                    {opptjening.pensjonspoeng[0].fnr}
+                    <ul>
+                        {opptjening.map(pensjonspoeng => (
+                            <li >
+                                pensjonspoeng.fnr
+                            </li>
+                        ))}
+                    </ul>
                 </header>
             </div>
         );
-    } else if (error) {
-        return (<div>{error}</div>);
-    } else {
-        return (<div>LOADING</div>);
     }
+
+    // if (isLoaded && opptjening && opptjening.pensjonspoeng && opptjening.pensjonspoeng[0]) {
+    //     return (
+    //         <div className="App">
+    //             <header className="App-header">
+    //                 <img src={logo} className="App-logo" alt="logo"/>
+    //                 <p>
+    //                     Edit <code>src/App.js</code> and save to reload.
+    //                 </p>
+    //                 <a
+    //                     className="App-link"
+    //                     href="https://reactjs.org"
+    //                     target="_blank"
+    //                     rel="noopener noreferrer"
+    //                 >
+    //                     Learn React
+    //                 </a>
+    //                 {opptjening.pensjonspoeng[0].fnr}
+    //             </header>
+    //         </div>
+    //     );
+    // } else if (error) {
+    //     return (<div>{error}</div>);
+    // } else {
+    //     return (<div>LOADING</div>);
+    // }
 }
 
 export default App;
