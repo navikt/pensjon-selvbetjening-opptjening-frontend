3fdf395f1d84f1adc2e70f4a6aaaf0ccae8737c7
Rittik Dey <dey.rittik@nav.no>
2020-07-01 11:47:31 +0200

    PL-1441: Separate api utility file

---
+++ src/api/api.js
@@ -0,0 +1,53 @@
+export function isDev() {
+    return process.env.NODE_ENV === 'development';
+}
+
+const RequestMethod = {
+    GET: "GET",
+    POST: "POST",
+    PUT: "PUT",
+    DELETE: "DELETE"
+};
+
+const serverRequest = (method, urlPath) => {
+    const OPTIONS = {
+        method: method,
+        credentials: getCredentialsParam()  //Check isDev or not
+    };
+
+    return new Promise((resolve, reject) => {
+        fetch(urlPath, OPTIONS)
+            .then((response) => {
+                verifyStatusSuccessOrRedirect(response);
+                resolve(response.json());
+            })
+            .catch((reason) => reject(reason));
+    });
+};
+
+function verifyStatusSuccessOrRedirect(response) {
+    // If we are on localhost just return, no need to check for authentication
+    if(isDev()){
+        return;
+    }
+    if (response.status === 401) {
+        window.location.href = "https://loginservice-q.nav.no/login?redirect=https://www-q0.nav.no/pensjon/opptjening/";
+        throw new Error("unauthorized");
+    }
+    if (response.status >= 200 && response.status < 300) {
+        return response.status;
+    }
+    throw new Error(response.statusText);
+}
+
+function getCredentialsParam() {
+    return isDev() ? "include" : "same-origin";
+}
+
+export function fetchToJson(urlPath) {
+    return serverRequest(RequestMethod.GET, urlPath, "");
+}
+
+export function fetchPost(urlPath, body) {
+    return serverRequest(RequestMethod.POST, urlPath, body);
+}
--- src/redux/opptjening/opptjeningSaga.js
+++ src/redux/opptjening/opptjeningSaga.js
@@ -1,46 +1,10 @@
 import { all, put, call, takeLatest } from 'redux-saga/effects';
 import {FETCH_OPPTJENING_STARTED, FETCH_OPPTJENING_SUCCESS, FETCH_OPPTJENING_FAILURE} from './opptjeningActions'
-
-const DEVELOPMENT = process.env.NODE_ENV === 'development';
-
-export const serverRequest = (method,urlPath) => {
-    const OPTIONS = {
-        method: method,
-        credentials: "include"  //Check isDev or not
-    };
-
-    return new Promise((resolve, reject) => {
-        fetch(urlPath, OPTIONS)
-            .then((response) => {
-                verifyStatusSuccessOrRedirect(response);
-                resolve(response.json());
-            })
-            .catch((reason) => reject(reason));
-    });
-};
-
-function fetchToJson(urlPath, withAccessToken) {
-    return serverRequest("GET", urlPath, "", withAccessToken);
-}
-
-function verifyStatusSuccessOrRedirect(response) {
-    // If we are on localhost just return, no need to check for authentication
-    if(DEVELOPMENT){
-        return;
-    }
-    if (response.status === 401) {
-        window.location.href = "https://loginservice-q.nav.no/login?redirect=https://www-q0.nav.no/pensjon/opptjening/";
-        throw new Error("unauthorized");
-    }
-    if (response.status >= 200 && response.status < 300) {
-        return response.status;
-    }
-    throw new Error(response.statusText);
-}
+import {fetchToJson} from "../../api/api";
 
 function* fetchOpptjening() {
     try {
-        const opptjening = yield call(fetchToJson, "/pensjon/opptjening/api/opptjening", true);
+        const opptjening = yield call(fetchToJson, "/pensjon/opptjening/api/opptjening");
         yield put({ "type": FETCH_OPPTJENING_SUCCESS, opptjening });
     } catch (error) {
         yield put({ "type": FETCH_OPPTJENING_FAILURE, error });
