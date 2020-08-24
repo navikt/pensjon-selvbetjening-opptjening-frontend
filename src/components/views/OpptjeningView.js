d9f035bb45c9c5cf867641cbd2b58663880840b7
Rittik Dey <dey.rittik@nav.no>
2020-08-24 10:01:23 +0200

    PL-1441: Added initial details view

--- src/components/views/OpptjeningView.js
+++ src/components/views/OpptjeningView.js
@@ -7,6 +7,20 @@ import Ekspanderbartpanel from 'nav-frontend-ekspanderbartpanel';
 import {LineChart} from '../elements/LineChart/LineChart';
 import './OpptjeningView.less';
 
+const detailRow = (props) => {
+    const amount = new Intl.NumberFormat('no-NB',
+        {
+            style: 'currency',
+            currency: 'NOK' ,
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 0
+        }).format(props.belop);
+
+    return(
+        <div>{props.label} {props.year}: {amount}</div>
+    )
+};
+
 export const OpptjeningView = () => {
     const { t } = useTranslation();
     const yearArray = useSelector(getYearArray);
@@ -14,7 +28,50 @@ export const OpptjeningView = () => {
     const latestPensjonsBeholdning = useSelector(getLatestPensjonsBeholdning);
 
     const [currentYear, setYear] = useState(latestPensjonsBeholdning.year);
-    const opptjeningByYear = useSelector(state => getOpptjeningByYear(state, currentYear));
+    const opptjening = useSelector(state => getOpptjeningByYear(state, currentYear));
+
+    const details = [];
+
+    if(opptjening && opptjening.endringOpptjening){
+        opptjening.endringOpptjening.forEach((endring) => {
+            let item;
+            if(endring.arsakType === "INNGAENDE"){
+                item = detailRow(
+                    {
+                        "label": "Beholdning fra",
+                        "year": (new Date(endring.dato)).getFullYear(),
+                        "belop": endring.pensjonsbeholdningBelop
+                    }
+                )
+            } else if(endring.arsakType === "OPPTJENING"){
+                item = detailRow(
+                    {
+                        "label": "Opptjening fra",
+                        "year": (new Date(endring.dato)).getFullYear() - 2,
+                        "belop": endring.endringBelop
+                    }
+                )
+            } else if(endring.arsakType === "REGULERING"){
+                item = detailRow(
+                    {
+                        "label": "Regulering",
+                        "year": (new Date(endring.dato)).getFullYear(),
+                        "belop": endring.endringBelop
+                    }
+                )
+            } else if(endring.arsakType === "UTTAK"){
+                item = detailRow(
+                    {
+                        "label": "Uttak",
+                        "year": (new Date(endring.dato)).getFullYear(),
+                        "belop": endring.endringBelop
+                    }
+                )
+            }
+            details.push(item);
+        });
+    }
+
 
     return(
         <div className="opptjeningBody">
@@ -31,7 +88,12 @@ export const OpptjeningView = () => {
                 <h2>{currentYear}</h2>
             </div>
             <Ekspanderbartpanel tittel={t("opptjening-what-happened-this-year")} border className="panelWrapper">
-                <pre id="json">{JSON.stringify(opptjeningByYear, null, 4)}</pre>
+                <div className="detailsBox">
+                    {details}
+                </div>
+            </Ekspanderbartpanel>
+            <Ekspanderbartpanel tittel="Data" className="panelWrapper">
+                <pre id="json">{JSON.stringify(opptjening, null, 4)}</pre>
             </Ekspanderbartpanel>
         </div>
     )
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,9 +3,11 @@ module.exports = function(app) {
     app.use(
         '/pensjon/opptjening/api/opptjening',
         createProxyMiddleware({
+            //target: 'http://localhost:8080/api/opptjening',
             target: 'http://localhost:4000/opptjening',
             changeOrigin: true,
             pathRewrite: {
+                //'^/pensjon/opptjening/api/opptjening' : '/'
                 '^/pensjon/opptjening/' : '/'
             }
         })
