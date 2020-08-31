b5c6b06bd10a6c1b67ffceddd0081f86e207cad1
Rittik Dey <dey.rittik@nav.no>
2020-08-31 15:36:02 +0200

    PL-1441: Using react-testing-library instead of enzyme, added tests for TopBanner, added i18next mock

--- src/components/elements/TopBanner/TopBanner.test.js
+++ src/components/elements/TopBanner/TopBanner.test.js
@@ -1,7 +1,17 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render } from '@testing-library/react';
 import {TopBanner} from './TopBanner';
 
-it('Should render without crashing', () => {
-    shallow(<TopBanner />);
+it('renders default frontpage topbanner', () => {
+    const { getByText, getByRole } = render(<TopBanner title="TOPBANNER_TITLE" text="TOPBANNER_TEXT"/>);
+    expect(getByText('TOPBANNER_TITLE')).toBeInTheDocument();
+    expect(getByText('TOPBANNER_TEXT')).toBeInTheDocument();
+    expect(getByRole('presentation')).toBeInTheDocument();
+});
+
+it('renders topbanner without illustration', () => {
+    const { getByText, queryByRole } = render(<TopBanner frontpage={false} showIllustration={false} title="TOPBANNER_TITLE" text="TOPBANNER_TEXT"/>);
+    expect(getByText('TOPBANNER_TITLE')).toBeInTheDocument();
+    expect(getByText('TOPBANNER_TEXT')).toBeInTheDocument();
+    expect(queryByRole('presentation')).toBeNull();
 });
--- src/containers/App/App.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import {App} from './App';
-
-it('Should render without crashing', () => {
-  shallow(<App />);
-});
