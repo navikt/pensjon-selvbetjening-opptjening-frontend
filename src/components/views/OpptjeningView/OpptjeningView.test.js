ed62573ac353eb3d656b19bd62f3fba140a6956b
Rittik Dey <dey.rittik@nav.no>
2020-10-23 12:18:42 +0200

    PL-1441: PL-2562: Fixed failing tests

--- src/components/views/OpptjeningView/OpptjeningView.test.js
+++ src/components/views/OpptjeningView/OpptjeningView.test.js
@@ -22,10 +22,9 @@ it('should render Opptjening view and display only the headings for the differen
     expect(view.queryAllByRole("heading")[0]).toHaveTextContent("opptjening-your-pension-assets-in-folketrygden"); //Beholdning-panel
     expect(view.queryAllByRole("heading")[1]).toHaveTextContent("pensjonsbeholdning-forklart"); //BeholdningForklart-panel
     expect(view.queryAllByRole("heading")[2]).toHaveTextContent("opptjening-increase-in-pension-assets-per-year"); //Chart - Chart-view
-    expect(view.queryAllByRole("heading")[3]).toHaveTextContent("opptjening-increase-in-pension-assets-per-year"); //Chart - Table-view
-    expect(view.queryAllByRole("heading")[4]).toHaveTextContent("opptjening-details-din-okning-ar-for-ar"); //OpptjeningDetails-panel
-    expect(view.queryAllByRole("heading")[5]).toHaveTextContent("opptjening-pensjonsgivende-inntekter"); //Inntekter-panel
-    expect(view.queryAllByRole("heading")[6]).toHaveTextContent("faq-ofte-stilte-sporsmaal"); //FAQ-panel
+    expect(view.queryAllByRole("heading")[3]).toHaveTextContent("opptjening-details-din-okning-ar-for-ar"); //OpptjeningDetails-panel
+    expect(view.queryAllByRole("heading")[4]).toHaveTextContent("opptjening-pensjonsgivende-inntekter"); //Inntekter-panel
+    expect(view.queryAllByRole("heading")[5]).toHaveTextContent("faq-ofte-stilte-sporsmaal"); //FAQ-panel
 
     expect(view.queryAllByRole("table").length).toBe(2); // Chart tables
 });
@@ -35,7 +34,7 @@ it('should render Opptjening view, open OpptjeningDetails-panel and show details
     let store = mockStore(mockedState);
 
     let view = render(<Provider store={store}><OpptjeningView/></Provider>);
-    fireEvent.click(view.queryAllByRole("heading")[4]); // OpptjeningDetails
+    fireEvent.click(view.queryAllByRole("heading")[3]); // OpptjeningDetails
 
     expect(view.queryAllByRole("table").length).toBe(3); // Chart tables + details tables
 });
@@ -45,7 +44,6 @@ it('should render Opptjening view, open Inntekter-panel and display inntekter ta
     let store = mockStore(mockedState);
 
     let view = render(<Provider store={store}><OpptjeningView/></Provider>);
-    fireEvent.click(view.queryAllByRole("heading")[5]); // Inntekter
-
+    fireEvent.click(view.queryAllByRole("heading")[4]); // Inntekter
     expect(view.queryAllByRole("table").length).toBe(3); // Chart tables + inntekter table
 });
