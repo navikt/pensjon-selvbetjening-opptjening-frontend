52e10f2e6421c263a6c43332a111474e55e21174
S159307 <swati.sharma@nav.no>
2020-11-05 22:06:57 +0100

    PL-1441: Fixed translationKeys script to check all files should have same keys

--- src/common/TranslationKeys.js
+++ src/common/TranslationKeys.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 /*This function returns true only if the keys of all the json files
-  for both languages englesk and nynorsk are same as bokmål norsk keys */
+  for all three languages are equal */
 export function compareJsonKeys(){
     const pathEN = '../../public/locales/en-GB';
     const pathNN = '../../public/locales/nn-NO';
@@ -10,20 +10,23 @@ export function compareJsonKeys(){
     const directoryPathToNB = path.resolve("./public/locales/nb-NO");
     const fileList = fs.readdirSync(directoryPathToNB);
 
-   return fileList.every(function (file) {
-       const checkEN = compareKeysForTwoFiles(pathNB + '/' + file, pathEN + '/' + file);
-       const checkNN = compareKeysForTwoFiles(pathNB + '/' + file, pathNN + '/' + file);
+    return fileList.every(function (file) {
+        const checkEN = compareKeysForTwoFiles(pathNB + '/' + file, pathEN + '/' + file);
+        const checkNN = compareKeysForTwoFiles(pathNB + '/' + file, pathNN + '/' + file);
 
-       return !(checkEN === false || checkNN === false);
-   });
+        return !(checkEN === false || checkNN === false);
+    });
 }
 
-/*  This function returns true if all keys of json file2 is in file1 */
+/*  This function returns true if all keys of json file1 and file2 are equal */
 export const compareKeysForTwoFiles = (file1Path, file2Path) =>{
     let json1 = require(file1Path);
     let json2 = require(file2Path);
     const keysJson1 = Object.keys(json1);
     const keysJson2 = Object.keys(json2);
-    return keysJson2.every(i => keysJson1.includes(i));
+    if(keysJson1.length === keysJson2.length){
+        return keysJson2.every(i => keysJson1.includes(i));
+    }else
+        return false;
 };
 
