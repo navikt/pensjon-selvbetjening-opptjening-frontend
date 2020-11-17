cf804fa0b7ed07a1a2d8137e0b700fd1a9603173
S159307 <swati.sharma@nav.no>
2020-11-17 15:14:46 +0100

    PL-1441: Removed info logging message

--- src/common/logging.js
+++ src/common/logging.js
@@ -6,12 +6,4 @@ import {
 
 export const logger = createFrontendLogger('pensjon-selvbetjening-opptjening-frontend', DEFAULT_FRONTENDLOGGER_API_URL);
 
-// Logging
-logger.info('Info');
-//logger.warn('Warn');
-//logger.error('Error');
-
-// Metrics
-//logger.event('navn-pa-metrikk', { field1: 'value1' }, { tag1: 'value2' });
-
 setUpErrorReporting(logger);
