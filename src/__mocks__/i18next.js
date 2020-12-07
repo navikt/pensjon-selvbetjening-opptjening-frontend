49e6355894cfd39320d529c8e2b1f74659ec55c9
R154508 <sindre.rasmussen@nav.no>
2020-12-07 11:24:55 +0100

    PL-1441: PL-2789: Fixed test for faq.

--- src/__mocks__/i18next.js
+++ src/__mocks__/i18next.js
@@ -2,6 +2,9 @@ module.exports = {
     getDataByLanguage: (lng) => {
         return {
             faq: (k) => k,
+            faq_born_after_1962: (k) => k,
+            faq_born_in_or_between_1943_and_1953: (k) => k,
+            faq_born_in_or_between_1954_and_1962: (k) => k,
             grunnlag: (k) => k,
             remarks: (k) => k,
             translation: (k) => k,
