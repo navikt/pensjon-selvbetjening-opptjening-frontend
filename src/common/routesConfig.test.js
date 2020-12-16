acc155f5ea578cc00a178c9168e0e415cca57809
swatisharma <swati.sharma@nav.no>
2020-12-16 10:25:10 +0100

    PL-2820: Fixed routesConfig.test.js and updated deploy-dev-sbs.yaml

--- src/common/routesConfig.test.js
+++ src/common/routesConfig.test.js
@@ -9,11 +9,11 @@ it('should return all defined routes', () => {
     expect(routesConfig.length).toEqual(4
     );
 
-    expect(routesConfig[0].path).toEqual("/");
+    expect(routesConfig[0].path).toEqual("/:lng/");
     expect(routesConfig[0].titleKey).toEqual("opptjening-tittel");
     expect(routesConfig[0].component).toEqual(HomePage);
 
-    expect(routesConfig[1].path).toEqual("/404");
+    expect(routesConfig[1].path).toEqual("/:lng/404");
     expect(routesConfig[1].titleKey).toEqual("404-title");
     expect(routesConfig[1].component).toEqual(NotFoundPage);
 });
