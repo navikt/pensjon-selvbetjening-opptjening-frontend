5e4688d7fb623ce0d9ea31c1cfa46d83c664f7d6
R154508 <sindre.rasmussen@nav.no>
2020-12-16 15:46:46 +0100

    PL-1441: PL-3003: Added test for OpptjeningFlereStederPanel

--- src/components/elements/OpptjeningFlereStederPanel/OpptjeningFlereStederPanel.test.js
+++ src/components/elements/OpptjeningFlereStederPanel/OpptjeningFlereStederPanel.test.js
@@ -1,2 +1,19 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import {OpptjeningFlereStederPanel} from './OpptjeningFlereStederPanel';
+import userEvent from "@testing-library/user-event";
+
+it('should render OpptjeningFlereStederPanel with title and explanation texts', () => {
+    render(<OpptjeningFlereStederPanel/>);
+    userEvent.click(screen.getByRole("heading"));
+
+    expect(screen.getByRole('heading')).toHaveTextContent("opptjening-flere-steder-title");
+    expect(screen.getByText("opptjening-flere-steder-forklart-title")).toBeInTheDocument();
+    expect(screen.getByText("opptjening-flere-steder-individuell")).toBeInTheDocument();
+    expect(screen.getByText("opptjening-flere-steder-tjenestepensjon")).toBeInTheDocument();
+    expect(screen.getByText("opptjening-flere-steder-folketrygden")).toBeInTheDocument();
+    expect(screen.getByText("opptjening-flere-steder-forklart")).toBeInTheDocument();
+
+});
 
 
