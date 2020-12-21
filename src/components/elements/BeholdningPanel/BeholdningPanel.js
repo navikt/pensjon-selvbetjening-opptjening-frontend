19f8fa299705b480274c2e16523964954a65ce41
Rittik Dey <dey.rittik@nav.no>
2020-12-21 15:40:40 +0100

    PL-1441: SVGs into separate files

--- src/components/elements/BeholdningPanel/BeholdningPanel.js
+++ src/components/elements/BeholdningPanel/BeholdningPanel.js
@@ -4,6 +4,7 @@ import {formatAmount} from "../../../common/utils";
 import Panel from "nav-frontend-paneler";
 import {Systemtittel} from "nav-frontend-typografi";
 import "./BeholdningPanel.less"
+import sparegris from "../../../assets/sparegris.svg"
 
 export const BeholdningPanel = (props) => {
     const { t } = useTranslation();
@@ -11,21 +12,7 @@ export const BeholdningPanel = (props) => {
     return(
         <Panel border>
             <div className="beholdningPanel">
-                <svg width="4rem" viewBox="0 0 71 89" fill="none" xmlns="http://www.w3.org/2000/svg" focusable="false" role="presentation" className="illustration">
-                    <path d="M38 20C43.5228 20 48 15.5228 48 10C48 4.47715 43.5228 0 38 0C32.4772 0 28 4.47715 28 10C28 15.5228 32.4772 20 38 20Z" fill="#FFBD66"/>
-                    <path d="M36.5 84C65.8848 84 68 73.2549 68 60.0001C68 46.7454 66.9282 34.0667 37.7891 34.0003C6.28906 33.9285 5 46.7454 5 60.0001C5 73.2549 7.11523 84 36.5 84Z" fill="#C86151"/>
-                    <path d="M13 59C14.1046 59 15 57.6569 15 56C15 54.3431 14.1046 53 13 53C11.8954 53 11 54.3431 11 56C11 57.6569 11.8954 59 13 59Z" fill="#3E3832"/>
-                    <path d="M2 56H8C9.10457 56 10 56.8954 10 58V68C10 69.1046 9.10457 70 8 70H2C0.89543 70 0 69.1046 0 68V58C0 56.8954 0.89543 56 2 56Z" fill="#C86151"/>
-                    <path d="M16.5522 29.7169L26 41H13L13 30.9407C13 29.8689 13.8954 29 15 29C15.6023 29 16.1724 29.2633 16.5522 29.7169Z" fill="#C86151"/>
-                    <path d="M17.7957 75L24 78.0646L18.9409 87.5757C18.2637 88.8489 16.7752 89.3551 15.5249 88.7375C14.2746 88.1199 13.6839 86.5867 14.1694 85.2188L17.7957 75Z" fill="#C86151"/>
-                    <path d="M13 70C15.7614 70 18 67.7614 18 65C18 62.2386 15.7614 60 13 60C10.2386 60 8 62.2386 8 65C8 67.7614 10.2386 70 13 70Z" fill="#D6897D"/>
-                    <path d="M56.2043 75L50 78.0646L55.0591 87.5757C55.7363 88.8489 57.2248 89.3551 58.4751 88.7375C59.7254 88.1199 60.3161 86.5867 59.8306 85.2188L56.2043 75Z" fill="#C86151"/>
-                    <path d="M68.5 64C69.8807 64 71 62.2091 71 60C71 57.7909 69.8807 56 68.5 56C67.1193 56 66 57.7909 66 60C66 62.2091 67.1193 64 68.5 64Z" fill="#C86151"/>
-                    <path d="M26 41.6712C26 40.7068 26.6955 39.8866 27.6512 39.7577C29.8457 39.4618 33.9229 39 38 39C42.0771 39 46.1543 39.4618 48.3488 39.7577C49.3045 39.8866 50 40.7068 50 41.6712C50 42.8903 48.9059 43.8261 47.6966 43.6723C45.4039 43.3807 41.7019 43 38 43C34.2981 43 30.5961 43.3807 28.3034 43.6723C27.0941 43.8261 26 42.8903 26 41.6712Z" fill="#3E3832"/>
-                    <path d="M31.1892 42C29.2272 40.2082 28 37.6517 28 34.814C28 29.3939 32.4772 25 38 25C43.5228 25 48 29.3939 48 34.814C48 37.6517 46.7728 40.2082 44.8108 42C42.5405 41.7892 40.2703 41.6839 38 41.6839C35.7297 41.6839 33.4595 41.7892 31.1892 42Z" fill="#FFBD66"/>
-                    <path fillRule="evenodd" clipRule="evenodd" d="M28.0494 36C28.5511 30.9467 32.8147 27 38 27C43.1853 27 47.4489 30.9467 47.9506 36C47.9833 35.6711 48 35.3375 48 35C48 29.4772 43.5228 25 38 25C32.4772 25 28 29.4772 28 35C28 35.3375 28.0167 35.6711 28.0494 36Z" fill="#D87F0A"/>
-                    <path fillRule="evenodd" clipRule="evenodd" d="M28.0494 11C28.5511 5.94668 32.8147 2 38 2C43.1853 2 47.4489 5.94668 47.9506 11C47.9833 10.6711 48 10.3375 48 10C48 4.47715 43.5228 0 38 0C32.4772 0 28 4.47715 28 10C28 10.3375 28.0167 10.6711 28.0494 11Z" fill="#D87F0A"/>
-                </svg>
+                <img src={sparegris} className="illustration" alt=""/>
                 <div className="content">
                     <Systemtittel id="pensjonsBeholdningTitle">{t('beholdning-din-pensjonsbeholdning-i-folketrygden')}</Systemtittel>
                     <div data-testid="assets" className="typo-sidetittel">kr {formatAmount(latestPensjonsBeholdning.beholdning)}</div>
--- src/components/elements/FAQPanel/FAQPanel.js
+++ src/components/elements/FAQPanel/FAQPanel.js
@@ -7,6 +7,7 @@ import "./FAQPanel.less";
 import Ekspanderbartpanel from "nav-frontend-ekspanderbartpanel";
 import {CLICK_EVENT, logToAmplitude} from "../../../common/amplitude";
 import {BORN_BEFORE_1943, BORN_IN_OR_BETWEEN_1943_AND_1953} from "../../../common/userGroups";
+import sporsmalstegn from "../../../assets/sporsmalstegn.svg";
 
 
 export const FAQPanel = (props) => {
@@ -57,13 +58,7 @@ export const FAQPanel = (props) => {
         <Panel border className="panelWrapper">
             <div className="faqTitleContainer">
                 <div className="faqTitle">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="1.2rem" viewBox="0 0 62 80" id="ark-hjelp" className="illustration" focusable="false" role="presentation">
-                        <g fill="none" fillRule="evenodd">
-                            <path fill="#E4E4DB" d="M0 13.322v62.635A4.036 4.036 0 0 0 4.029 80h53.944A4.035 4.035 0 0 0 62 75.957V4.043A4.035 4.035 0 0 0 57.973 0H14.021L0 13.322z"></path>
-                            <path fill="#C9C9C9" d="M14.149 0v9.285c0 2.235-1.82 4.046-4.066 4.046H0L14.149 0z"></path>
-                            <path fill="#669DB4" d="M27.15 47.262c-.204-1.452-.153-2.784.153-3.994.305-1.21.753-2.32 1.344-3.329a18.648 18.648 0 0 1 2.017-2.814c.754-.867 1.467-1.714 2.139-2.541a16.988 16.988 0 0 0 1.68-2.451 5.275 5.275 0 0 0 .673-2.603c0-1.452-.438-2.622-1.314-3.51-.876-.887-2.088-1.331-3.636-1.331-1.263 0-2.404.282-3.423.847-1.018.565-1.976 1.331-2.872 2.3L20 24.266a16.88 16.88 0 0 1 4.797-3.783C26.651 19.494 28.74 19 31.061 19c1.589 0 3.056.212 4.4.635 1.345.424 2.495 1.06 3.453 1.907.957.847 1.711 1.896 2.261 3.147.55 1.25.825 2.703.825 4.357 0 1.291-.234 2.451-.703 3.48a15.947 15.947 0 0 1-1.741 2.935 30.988 30.988 0 0 1-2.231 2.663 21.928 21.928 0 0 0-2.139 2.663 13.651 13.651 0 0 0-1.528 2.965c-.387 1.05-.499 2.22-.336 3.51H27.15zM30.267 61c-1.304 0-2.384-.444-3.24-1.331-.855-.888-1.283-2.018-1.283-3.39 0-1.371.428-2.5 1.284-3.389.855-.887 1.935-1.33 3.239-1.33 1.303 0 2.393.443 3.27 1.33.875.888 1.313 2.018 1.313 3.39 0 1.371-.438 2.501-1.314 3.389-.876.887-1.966 1.331-3.27 1.331z"></path>
-                        </g>
-                    </svg>
+                    <img src={sporsmalstegn} className="illustration" alt=""/>
                     <Undertittel id="faqTitle" className="lenkepanel__heading title">{t('faq-ofte-stilte-sporsmaal')}</Undertittel>
                 </div>
             </div>
