a8e8e85ddc0840464e83796e37f9543223c3d1a3
Rittik Dey <dey.rittik@nav.no>
2021-02-02 17:18:57 +0100

    PL-1441: PL-3203: Handling the different http status codes

--- src/containers/OpptjeningContainer/OpptjeningContainer.js
+++ src/containers/OpptjeningContainer/OpptjeningContainer.js
@@ -8,6 +8,7 @@ import Alertstripe from "nav-frontend-alertstriper";
 import * as urlHelper from "../../common/urlHelper";
 import "./OpptjeningContainer.less"
 import Lenke from "nav-frontend-lenker";
+import {LoginPanel} from "../../components/elements/LoginPanel/LoginPanel";
 
 export const OpptjeningContainer = (props) => {
     const { t } = useTranslation();
@@ -28,24 +29,33 @@ export const OpptjeningContainer = (props) => {
 
     if(opptjeningError){
         if(opptjeningError.message === "error-status-401") {
-            //401 - Unauthorized - go directly to login - no error message - no "blink"
-            return <></>
+            //401 - Unauthorized - Click to login
+            return (
+                <main className="contentWrapper">
+                    <LoginPanel/>
+                </main>
+            )
         } else if (opptjeningError.message === "error-status-418"){
             // 418 - I'm a teapot - to be removed - temporarily redirect to old solution for some users we are missing functionality for (i.e. uttak)
+            // TODO: Remove when missing functionality is implemented
             return (
-                <div className="contentWrapper" data-testid="opptjening-error">
-                    <Alertstripe type="advarsel">
-                        {t(opptjeningError.message)}
-                        <br/>
-                        <Lenke href={urlHelper.DINEPENSJONSPOENG_URL}>Dine pensjonspoeng</Lenke>
-                    </Alertstripe>
-                </div>
+                <main className="contentWrapper">
+                    <div data-testid="opptjening-error">
+                        <Alertstripe type="advarsel">
+                            {t(opptjeningError.message)}
+                            <br/>
+                            <Lenke href={urlHelper.DINEPENSJONSPOENG_URL}>{t('error-status-418-link-title')}</Lenke>
+                        </Alertstripe>
+                    </div>
+                </main>
             )
         } else {
             return (
-                <div className="contentWrapper" data-testid="opptjening-error">
-                    <Alertstripe type="feil">{t(opptjeningError.message)}</Alertstripe>
-                </div>
+                <main className="contentWrapper">
+                    <div data-testid="opptjening-error">
+                        <Alertstripe type="feil">{t(opptjeningError.message)}</Alertstripe>
+                    </div>
+                </main>
             )
         }
     }
