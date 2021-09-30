9ada7ea6365425df690053b6cbc719339d0ad0f6
Rittik Dey <dey.rittik@nav.no>
2021-09-30 16:11:17 +0200

    PL-1441: PL-3816: Changed description for test

--- src/components/elements/BeholdningPanel/BeholdningPanel.test.js
+++ src/components/elements/BeholdningPanel/BeholdningPanel.test.js
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import {BeholdningPanel} from './BeholdningPanel';
 import {formatAmount} from "../../../common/utils";
 
-it('renders the Beholdning panel with correct heading and correct formatted assets', () => {
+it('renders the Beholdning panel with correct heading and correct amount for assets', () => {
     const panel = render(<BeholdningPanel data={{'beholdning': "1200000"}}/>);
 
     expect(panel.getByRole("heading")).toHaveTextContent("beholdning-din-pensjonsbeholdning-i-folketrygden");
