c59e84710542b43623e4740c2514d508f9d472eb
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2021-10-01 08:54:19 +0200

    Locale identification github actions

--- src/components/elements/BeholdningPanel/BeholdningPanel.test.js
+++ src/components/elements/BeholdningPanel/BeholdningPanel.test.js
@@ -8,4 +8,8 @@ it('renders the Beholdning panel with correct heading and correct amount for ass
 
     expect(panel.getByRole("heading")).toHaveTextContent("beholdning-din-pensjonsbeholdning-i-folketrygden");
     expect(panel.getByTestId("assets").textContent).toEqual("kr " + formatAmount(1200000));
+    console.log(formatAmount(1200000));
+
+    console.log('SUPPORTED LOCALES', Intl.NumberFormat
+        .supportedLocalesOf(['de-DE', 'nb-NO', 'no-NO', 'en-US']));
 });
