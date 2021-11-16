f5df1c6b93fe6677791c40c94ed22900bde5e6f8
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2021-11-16 15:13:39 +0100

    PL-4634 request localhost devserver med assertions

--- cypress/integration/opptjeningforside_hele_spec.js
+++ cypress/integration/opptjeningforside_hele_spec.js
@@ -1,18 +1,39 @@
 describe('Opptjening forside', () => {
     it('vg', () => {
         cy.viewport(1000, 660); //default
-        cy.visit('https://www.google.no///');
+        cy.visit('https://www.google.no');
         cy.matchImageSnapshot({
             capture: 'fullPage',
         });
     })
     it('cypress', () => {
         cy.viewport(1000, 660); //default
+        /*cy.request('http://127.0.0.1:3000/pensjon/opptjening')
+            .then((response) => {
+                expect(response.status).to.eq(200)
+                expect(response.body).to.have.length(500)
+                expect(response).to.have.property('headers')
+                expect(response).to.have.property('duration')
+                console.log('request RESPONSE', response);
+            })*/
+
+        cy.request('http://127.0.0.1:4000/opptjening')
+            .then((response) => {
+            expect(response.status).to.eq(200)
+            //expect('http://127.0.0.1:4000/opptjening').to.eq(response.statusText);
+            expect({}).to.deep.equal(response.body)
+            expect(response).to.have.property('headers')
+            expect(response).to.have.property('duration')
+            console.log('request RESPONSE', response);
+        })
+
+
         cy.visit('http://127.0.0.1:3000/pensjon/opptjening', {
             onBeforeLoad(win) {
                 cy.stub(win.console, 'log').as('consoleLog')
                 cy.stub(win.console, 'error').as('consoleError')
-            }
+            },
+            log: true,
         }) // change URL to match your dev URL
 
         cy.get("#forklartseksjon").click();
