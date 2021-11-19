b4a0bf95c20810ce1b84e71147e74c344be26531
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2021-11-19 17:32:04 +0100

    PL-4634 Remove old image with border

--- cypress/integration/opptjeningforside_hele_spec.js
+++ cypress/integration/opptjeningforside_hele_spec.js
@@ -19,7 +19,8 @@ describe('Opptjening forside', () => {
 
         cy.get("#opptjening-flere-steder-forklart").should("be.visible");
         cy.get("#opptjening-flere-steder-forklart").contains("Alle får pensjon i folketrygden. Hvis du er usikker på om du har individuell sparing eller tjenestepensjon kan du for eksempel se hos banken din eller spørre nåværende eller tidligere arbeidsgivere.");
-        cy.get("#opptjening-flere-steder-forklart").click();
+        cy.get("#opptjening-flere-steder-forklart").click({ scrollBehavior: false });
+
         cy.matchImageSnapshot({
             capture: 'fullPage',
         });
