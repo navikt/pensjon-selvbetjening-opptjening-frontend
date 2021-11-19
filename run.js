126588490289b447ae12f4012a7bfbeb6167a7e2
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2021-11-19 13:13:30 +0100

    PL-4634 Only one build script

--- run.js
+++ run.js
@@ -6,7 +6,7 @@ let filename = args.length > 0 ? args[0] : "mock.json";
 concurrently(
     [
         { command: 'npm:start HOST=127.0.0.1', prefixColor: 'blue', name: 'pensjon-selvbetjening-opptjening' },
-        { command: 'json-server --host 127.0.0.1 --port 4000 --watch dev/' + filename, prefixColor: 'magenta', name: 'json-server' }
+        { command: 'json-server --port 4000 --watch dev/' + filename, prefixColor: 'magenta', name: 'json-server' }
     ],
     {
         killOthers: ['failure', 'success']
