3a74666dee027fe9acb0f7fcaab6b3ef379d0ecf
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2021-11-22 13:30:22 +0100

    PL-4634 Update baseline with intercepted auth for dekoratøren=true

--- cypress/integration/opptjeningforside_hele_spec.js
+++ cypress/integration/opptjeningforside_hele_spec.js
@@ -1,5 +1,13 @@
 describe('Opptjening forside', () => {
     it('forside', () => {
+        cy.intercept('https://innloggingsstatus.dev.nav.no/person/innloggingsstatus/auth', {
+            statusCode: 200,
+            body: {
+                "authenticated": true,
+                "name": "Test",
+                "securityLevel": "4"
+            },
+        });
         cy.viewport(1000, 660); //default
         cy.request('/pensjon/opptjening')
             .then((response) => {
@@ -10,7 +18,6 @@ describe('Opptjening forside', () => {
         cy.visit('/pensjon/opptjening', {
             log: true,
         });
-
         cy.get("#forklartseksjon").click();
         cy.get("#inntektmedmerknadpanel").click();
         cy.get("#din-okning-aar-for-aar").click();
@@ -21,7 +28,7 @@ describe('Opptjening forside', () => {
         cy.get("#opptjening-flere-steder-forklart").click();
 
         cy.get("#opptjening-flere-steder").should('have.attr', 'animation-finished', 'true');//hvis man noen gang må fjerne denne pga bumping kan man legge til en wait hvis man ikke finner ut av en tilsvarende metode. Merk: anti-patterin
-        cy.get("#opptjening-flere-steder-forklart").click();
+
         cy.get("#opptjening-flere-steder-forklart").contains("Alle får pensjon i folketrygden. Hvis du er usikker på om du har individuell sparing eller tjenestepensjon kan du for eksempel se hos banken din eller spørre nåværende eller tidligere arbeidsgivere.");
         cy.focused().blur()
 
--- cypress/integration/tabell_visning_spec.js
+++ cypress/integration/tabell_visning_spec.js
@@ -1,5 +1,13 @@
 describe('Opptjening forside tabell', () => {
     it('tabell', () => {
+        cy.intercept('https://innloggingsstatus.dev.nav.no/person/innloggingsstatus/auth', {
+            statusCode: 200,
+            body: {
+                "authenticated": true,
+                "name": "Test",
+                "securityLevel": "4"
+            },
+        });
         cy.viewport(1000, 660); //default
         cy.visit('/pensjon/opptjening') // change URL to match your dev URL
 
