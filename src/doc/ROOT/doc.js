efddd9fc82a84784aac40765362d0317bca2f87c
Sindre Rasmussen <sindre.rasmussen@nav.no>
2022-07-07 11:53:55 +0200

    PL-3066: Moved documentation files and documentation images.

--- src/doc/ROOT/doc.js
+++
@@ -1,3 +0,0 @@
-const asciidoctor = require('asciidoctor')();
-asciidoctor.convertFile("src/doc/ROOT/doc.adoc", { to_dir:'public/doc', to_file: 'index.html', standalone: true, safe: 'unsafe', mkdirs:true});
-
