883156e7e26b62aff2618126ec872cb0d7182d10
Sæbø <gard.sebo@nav.no>
2024-08-16 11:50:49 +0200

    Liten fiks

--- src/common/urlHelper.js
+++ src/common/urlHelper.js
@@ -5,6 +5,3 @@ export const BYTT_BRUKER_ENDPOINT = "/api/byttbruker";
 export const DINPENSJON_URL = process.env.REACT_APP_DINPENSJON_URL + queryParams;
 export const PENSJONSKALKULATOR_URL = process.env.REACT_APP_PENSJONSKALKULATOR_URL + queryParams;
 export const OVERFORE_OMSORGSOPPTJENING_URL = process.env.REACT_APP_OVERFORE_OMSORGSOPPTJENING_URL + queryParams;
-
-
-
